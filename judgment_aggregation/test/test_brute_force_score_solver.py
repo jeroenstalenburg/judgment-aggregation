57901cbf872bb7149b35095a1fc961224ab2ada4
Jeroen Stalenburg <jeroenstalenburg@gmail.com>
2020-04-27 23:32:31 +0200

    added more tests

---
+++ judgment_aggregation/test/test_brute_force_score_solver.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+
+from ..solvers.BruteForceScoreSolver import BruteForceScoreSolver
+from ..data.Scenario import Scenario
+
+
+class TestBruteForceScoreSolver(TestCase):
+    def test_kemeny_solve(self):
+        scenario = Scenario()
+        scenario.load_file("kemenyslaterdiff.ja")
+        solver = BruteForceScoreSolver(scenario)
+        solver.solve(procedure="Kemeny")
+        self.assertEqual(solver.get_answers(), [[0, 1, 1, 0, 0, 0, 1, 1]])
+
+    def test_slater_solve(self):
+        scenario = Scenario()
+        scenario.load_file("kemenyslaterdiff.ja")
+        solver = BruteForceScoreSolver(scenario)
+        solver.solve(procedure="Slater")
+        self.assertEqual(solver.get_answers(), [[1, 0, 0, 0, 0, 0, 1, 1]])
--- judgment_aggregation/test/test_cnf.py
+++ judgment_aggregation/test/test_cnf.py
@@ -4,14 +4,24 @@ from ..data.constraints.CNF import CNF
 
 
 class TestCNF(TestCase):
-    def test_create_cnf(self):
-        cnf = CNF()
-        cnf.load_file("notall3.cnf")
-        self.assertIsNotNone(cnf, "Creation of CNF constraint failed")
-
     def test_valid_agenda_generation(self):
         cnf = CNF()
         cnf.load_file("notall3.cnf")
-        self.assertEqual(list(cnf.generate_all_valid_agendas()),
-                         [[0, 0, 0], [0, 0, 1], [0, 1, 0], [0, 1, 1],
-                          [1, 0, 0], [1, 0, 1], [1, 1, 0]])
+        counter = 0
+        valids = [[0, 0, 0], [0, 0, 1], [0, 1, 0], [0, 1, 1], [1, 0, 0],
+                  [1, 0, 1], [1, 1, 0]]
+        for sol in cnf.generate_all_valid_agendas():
+            self.assertIn(sol, valids)
+            counter += 1
+        self.assertEqual(counter, 7, "not all valid solutions found")
+
+    def test_check_agenda(self):
+        cnf = CNF()
+        cnf.load_file("notall3.cnf")
+        self.assertFalse(cnf.check_agenda([1, 1, 1]))
+        self.assertTrue(cnf.check_agenda([1, 1, 0]))
+
+    def test_larger_file(self):
+        cnf = CNF()
+        cnf.load_file("large_test.cnf")
+        self.assertNotEqual(list(cnf.generate_all_valid_agendas()), [])
