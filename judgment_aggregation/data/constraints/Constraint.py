64ec57517524c70c43f5f7b3861f81c44c1794fe
Jeroen Stalenburg <jeroenstalenburg@gmail.com>
2020-04-27 23:45:18 +0200

    beter way to generate all possible agendas

--- judgment_aggregation/data/constraints/Constraint.py
+++ judgment_aggregation/data/constraints/Constraint.py
@@ -1,24 +1,26 @@
 from ..Data import Data
-from numpy import unpackbits
+import itertools
 
 
 class Constraint(Data):
     def check_agenda(self, agenda):
-        """"""
+        """Given a agenda in list form (e.g. [1, 1, 0]) return true or false
+        if valid according to the data"""
         raise NotImplementedError(
             "'check_agenda' not implemented for current constraint class %s" %
             type(self).__name__)
 
+        def get_var_amount(self):
+            raise NotImplementedError()
+
     def generate_all_valid_agendas(self):
-        """"""
+        """Generate all valid agendas according to the given constraint"""
         self.check_initialised()
-        if getattr(self, 'var_amount', -1) == -1:
-            self.throw_error("The amount of agenda variables has not been set")
-        agenda = [1] * self.var_amount
-        powers = [2**i for i in range(self.var_amount)][::-1]
-        for i in range(2**self.var_amount):
-            for power, index in zip(powers, range(self.var_amount)):
-                if i % power == 0:
-                    agenda[index] ^= 1
+        var_amount = self.get_var_amount()
+        if var_amount == -1:
+            self.throw_error("The amount of agenda variables has not been set"
+                             "; A constraint should have a var_amount "
+                             "attribute")
+        for agenda in map(list, itertools.product([0, 1], repeat=var_amount)):
             if self.check_agenda(agenda):
                 yield agenda.copy()
